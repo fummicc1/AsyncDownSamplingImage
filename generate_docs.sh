67f0dedb09820123842c675fb135de117616962f
fummicc1 <fumiya.tennis1@gmail.com>
2023-02-06 01:27:50 +0900

    Update docs

--- generate_docs.sh
+++ generate_docs.sh
@@ -2,6 +2,7 @@
 
 swift package --allow-writing-to-directory docs \
     generate-documentation --target AsyncDownSamplingImage \
+    --disable-indexing \
     --transform-for-static-hosting \
     --hosting-base-path asyncdownsamplingimage \
     --output-path docs
